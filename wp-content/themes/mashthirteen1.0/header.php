f02b10c12877b1f349179e5944256d5c501fbd9b
rhamick <rhamick@gmail.com>
2014-12-01 07:55:14 -0300

    adjusts css search google

--- wp-content/themes/mashthirteen1.0/header.php
+++ wp-content/themes/mashthirteen1.0/header.php
@@ -24,6 +24,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link media="all" rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/gcs.css" type="text">
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/html5shiv.js" type="text/javascript"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/respond.min.js" type="text/javascript"></script>
@@ -46,7 +47,30 @@
 	    			<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'mashthirteen' ); ?>"><?php _e( 'Skip to content', 'mashthirteen' ); ?></a>
 	    			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 	    			<?php if ( get_theme_mod( 'mashthirteen_menusearch_visibility' ) != 1 ) { ?>
-	    			<?php get_search_form(); ?>		
+	    			<?php // get_search_form(); ?>
+	    		
+						<div class="gcs_container pull-right">
+							<div id='cse' style='width: 100%;'>Loading</div>
+								<script src='//www.google.com/jsapi' type='text/javascript'></script>
+								<script type='text/javascript'>
+								google.load('search', '1', {language: 'en', style: google.loader.themes.V2_DEFAULT});
+								google.setOnLoadCallback(function() {
+								var customSearchOptions = {};
+								var orderByOptions = {};
+								orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
+								customSearchOptions['enableOrderBy'] = true;
+								customSearchOptions['orderByOptions'] = orderByOptions;
+								customSearchOptions['overlayResults'] = true;
+								var customSearchControl =   new google.search.CustomSearchControl('016369008610948135852:lv4-rycswky', customSearchOptions);
+								customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+								var options = new google.search.DrawOptions();
+								options.setAutoComplete(true);
+								customSearchControl.draw('cse', options);
+								}, true);
+								</script>
+							</div>
+						</div>
+
 	    			<?php } ?>
 	    		</nav><!-- #site-navigation -->
 	    	</div>
