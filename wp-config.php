e724529ecc73db90e0b5e3496fe5f3a9bb620d5c
Guillermo <guillefd@gmail.com>
2015-02-21 10:00:33 -0300

    fix

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'rwwpblog');
+define('DB_NAME', 'rewritem_blogwp');
 
 /** MySQL database username */
 define('DB_USER', 'root');
